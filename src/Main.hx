009db0d72f1ad9f171b88600cdedf8606a80f902
GuillaumeMasselot <guillaume.masselot@gmail.com>
2017-08-27 11:31:49 +0200

    Init project and first visual

---
+++ src/Main.hx
@@ -0,0 +1,71 @@
+package ;
+
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.Lib;
+
+class Main extends Sprite 
+{
+	var inited:Bool;
+	
+	private var platform1:Platform;
+	private var platform2:Platform;
+	private var ball:Ball;
+
+	/* ENTRY POINT */
+	
+	function resize(e) 
+	{
+		if (!inited) init();
+		// else (resize or orientation change)
+	}
+	
+	function init() 
+	{
+		if (inited) return;
+		inited = true;
+		
+		platform1 = new Platform();
+		platform1.x = 5;
+		platform1.y = 200;
+		this.addChild(platform1);
+		
+		platform2 = new Platform();
+		platform2.x = 480;
+		platform2.y = 200;
+		this.addChild(platform2);
+		
+		ball = new Ball();
+		ball.x = 250;
+		ball.y = 250;
+		this.addChild(ball);
+	}
+
+	/* SETUP */
+
+	public function new() 
+	{
+		super();	
+		addEventListener(Event.ADDED_TO_STAGE, added);
+	}
+
+	function added(e) 
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, added);
+		stage.addEventListener(Event.RESIZE, resize);
+		#if ios
+		haxe.Timer.delay(init, 100); // iOS 6
+		#else
+		init();
+		#end
+	}
+	
+	public static function main() 
+	{
+		// static entry point
+		Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		Lib.current.addChild(new Main());
+		//
+	}
+}
